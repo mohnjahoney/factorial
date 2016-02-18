8d36a96b1e51b715c367fc608d1f7923705b0e55
John Mahoney <mohnjahoney@gmail.com>
2016-02-17 16:38:44 -0800

    filled in setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
-# some setup script
-print("no setup script yet")
+from distutils.core import setup
+
+setup(
+    name='Factorial Package',
+    version='0.1dev',
+    packages=['factorial',],
+    license='GPL3',
+    long_description=open('README.md').read(),
+)
